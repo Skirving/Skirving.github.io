6414f329eead352f1cdc793dd34c3f557997421d
Skirving <skrcol001@myuct.ac.za>
2026-08-12 12:15:08 +0200

    Log profile image element before updating source
    Added console log for profile image element before updating the source.

--- js/header-manager.js
+++ js/header-manager.js
@@ -6,9 +6,10 @@ export class HeaderManager {
         const githubUsername = config.github_username || this.extractGithubUsername(config.social_links);
         
         // Update profile image
+        console.log('Profile image element:', document.querySelector('.profile-img'));
         if (githubUsername) {
             //document.querySelector('.profile-img').src = `https://avatars.githubusercontent.com/${githubUsername}?s=320`;
-            document.querySelector('.profile-img').src = `/assets/logos/profile.jpg`;
+            document.querySelector('.profile-img').src = '/assets/logos/profile.jpg';
         }
         
         // Update header text
